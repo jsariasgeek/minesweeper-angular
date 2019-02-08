58108404c8a79176774f26f1767da54c8c586e64
Johán Steven Arias <jsarias1993@gmail.com>
2019-02-08 15:29:54 -0500

    user can set parameters for board

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,13 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  rows = Array(9)
-  cols = Array(9)
+  numRows = 9
+  numCols = 9
+  numMines = 9
+
+  rows = Array(this.numRows);
+  cols = Array(this.numCols);
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.rows);
   }
+
+  setRows(){
+    this.rows = Array(this.numRows);
+  }
+
+  setCols(){
+    this.cols = Array(this.numCols);
+  }
+
+
+  exposeCell(){}
 
 }
